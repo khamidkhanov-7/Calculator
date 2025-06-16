9cbff611ddfd3e85c0ed7ebaab6923f68f7fd0df
khamidkhanov-7 <valihon555@gmail.com>
2025-06-16 16:42:46 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/calculate", (req, res) => {
+  const { num1, num2, operation } = req.body;
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    return res.json({ result: "Iltimos, son kiriting!" });
+  }
+
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      result = num2 !== 0 ? num1 / num2 : "0 ga bo‘lish mumkin emas!";
+      break;
+    default:
+      result = "Noto‘g‘ri amal!";
+  }
+
+  res.json({ result });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server ishlayapti: http://localhost:${PORT}`);
+});
